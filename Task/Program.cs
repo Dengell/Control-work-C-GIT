344febed4abb22061d14d6ffc51f351750e76547
Dengell <9680004050@mail.ru>
2023-04-18 16:24:42 +0300

    Исправил программу. Вывод корректный

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,25 +3,47 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-void Definition(string[] arr)
+// void Definition(string[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         string element = arr[i];
+//         if (element.Length <= 3)
+//         {
+//             arr[i] = element;
+//     }
+//         else arr[i] = string.Empty;
+//     }
+// }
+string[] Definition(string[] arr)
 {
+    int size = 0;
     for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3) size++;
+    }
+    string[] newarr = new string[size];
+    for (int i = 0, j = 0; i < arr.Length; i++)
     {
         string element = arr[i];
         if (element.Length <= 3)
         {
-            arr[i] = element;
+            newarr[j] = element; j++;
+        }
     }
-        else arr[i] = string.Empty;
-    }
+    return newarr;
 }
 
 void PrintArray(string[] arr)
 {
+    Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write($"{arr[i]} ");
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
     }
+    Console.Write("]");
 }
 
 string[] array = new string[] { "Hello", "2", "World", ":-)" };
@@ -29,6 +51,6 @@ string[] array = new string[] { "Hello", "2", "World", ":-)" };
 // string[] array = new string[] { "Russia", "Denmark", "Kazan" };
 
 PrintArray(array);
-Definition(array);
+string[] definition = Definition(array);
 Console.Write("   ->   ");
-PrintArray(array);
+PrintArray(definition);
